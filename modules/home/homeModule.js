0a023b244f3a9e08a31ee6c5b7d7cb7282cd044b
Arturo Rubio <arturo.rubio.89.8@gmail.com>
2016-08-22 18:04:52 +0200

    specifications done

--- modules/home/homeModule.js
+++ modules/home/homeModule.js
@@ -4,6 +4,9 @@ angular.module( 'homeModule', [ ] )
 		$scope.infoFilmSearched = "";
 
 		$scope.submit = function() {
+			$('.list-films').css('display', 'flex')
+			$('.insert-text').html('<h1>You have searched: <span class="item-searched">'+ $scope.filmToSearch + '</span></h1>')
+
 			homeService.getInfoFilm( $scope.filmToSearch )
 				.then( function ( dataFilmSearched ){
 					$scope.infoFilmSearched = dataFilmSearched.data.results;
@@ -12,28 +15,32 @@ angular.module( 'homeModule', [ ] )
 		}
 
 		$scope.searchFilmByMood = function( mood ) {
+			$('.list-films').css('display', 'flex')
+			$('.insert-text').html('<h1>You have searched: <span class="item-searched">' + mood + '</span></h1>')
 
 			console.log(mood)
-			var randomPage = Math.floor(Math.random() * 1000) + 1;
+			var randomPage1 = Math.floor(Math.random() * 1000) + 1;
+			var randomPage2 = Math.floor(Math.random() * 1000) + 1;
 			var moodNumber;
 
 			if ( mood === 'Sad' ) {
-				moodNumber = '18|10752|80';
+				moodNumber = '80|18|10752';
 			} else if ( mood === 'Joyful') {
-				moodNumber = '37';
+				moodNumber = '12|16|35|10751|14|10402|10749';
 			} else if ( mood === 'Mad') {
-				moodNumber = '99';
+				moodNumber = '28|80|53|10752';
 			} else if ( mood === 'Powerful') {
-				moodNumber = '10751';
+				moodNumber = '28|12|14|9648|878|53';
 			} else if ( mood === 'Scared') {
-				moodNumber = '10769';
+				moodNumber = '80|27|9648';
 			} else if ( mood === 'Peaceful') {
-				moodNumber = '10402';
+				moodNumber = '99|10751|14|36|10402|10749';
 			}
 
-			homeService.getInfoFilmByMood( moodNumber, randomPage )
+			homeService.getInfoFilmByMood( moodNumber, randomPage1, randomPage2 )
 				.then( function ( dataFilmSearched ){
-					$scope.infoFilmSearched = dataFilmSearched.data.results;
+					console.log(dataFilmSearched)
+					$scope.infoFilmSearched = dataFilmSearched[0].data.results;
 					console.log($scope.infoFilmSearched);
 				})
 		}
--- modules/home/homeService.js
+++ modules/home/homeService.js
@@ -1,8 +1,9 @@
 angular.module( 'homeService', [ ] )
-	.factory('homeService', function( $http ) {
+	.factory('homeService', function( $http, $q ) {
 
 		var urlToSearchFilm = 'http://api.themoviedb.org/3/search/movie?api_key=71bd8c83c5cc06c197435d2165ac52e4&query=<SEARCH>';
-		var urlToSearchFilmByMood = 'https://api.themoviedb.org/3/discover/movie?api_key=71bd8c83c5cc06c197435d2165ac52e4&with_genres=<MOOD_NUM>&page=<PAGE>';
+		var urlToSearchFilmByMood1 = 'https://api.themoviedb.org/3/discover/movie?api_key=71bd8c83c5cc06c197435d2165ac52e4&with_genres=<MOOD_NUM>&page=<PAGE1>';
+		var urlToSearchFilmByMood2 = 'https://api.themoviedb.org/3/discover/movie?api_key=71bd8c83c5cc06c197435d2165ac52e4&with_genres=<MOOD_NUM>&page=<PAGE2>';
 
 		function getInfoFilm ( filmToSearch ) {
 			var urlToSearchFilmChanged = urlToSearchFilm.replace('<SEARCH>', filmToSearch)
@@ -10,15 +11,28 @@ angular.module( 'homeService', [ ] )
 			return $http.get( urlToSearchFilmChanged )
 		}
 
-		function getInfoFilmByMood ( moodNumber, randomPage ) {
+		function getInfoFilmByMood ( moodNumber, randomPage1, randomPage2 ) {
 
-			var mapObj = { '<MOOD_NUM>':moodNumber, '<PAGE>':randomPage };
+			var mapObj = {
+				'<MOOD_NUM>': moodNumber,
+				'<PAGE1>': randomPage1,
+				'<PAGE2>': randomPage2
+			};
 			console.log(mapObj)
-			var urlToSearchFilmByMoodChanged = urlToSearchFilmByMood.replace(/<MOOD_NUM>|<PAGE>/gi, function(matched){
+			var urlToSearchFilmByMoodChanged1 = urlToSearchFilmByMood1.replace(/<MOOD_NUM>|<PAGE1>/gi, function(matched){
 				return mapObj[matched];
 			});
-			console.log(urlToSearchFilmByMoodChanged)
-			return $http.get( urlToSearchFilmByMoodChanged )
+			var urlToSearchFilmByMoodChanged2 = urlToSearchFilmByMood2.replace(/<MOOD_NUM>|<PAGE2>/gi, function(matched){
+				return mapObj[matched];
+			});
+			console.log(urlToSearchFilmByMoodChanged1)
+			console.log(urlToSearchFilmByMoodChanged2)
+
+
+			var promise1 = $http.get( urlToSearchFilmByMoodChanged1 )
+			var promise2 = $http.get( urlToSearchFilmByMoodChanged2 )
+			
+			return $q.all( [promise1, promise2] )
 		}
 
 		return {
