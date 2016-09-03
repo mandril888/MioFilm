2c0ad5f6d05db476cdcd74ea06ff297e657ff2b5
Arturo Rubio <arturo.rubio.89.8@gmail.com>
2016-09-03 21:33:31 +0200

    films not repeat in films watched server

--- server/app/functions/infoFilmSeen.js
+++ server/app/functions/infoFilmSeen.js
@@ -9,14 +9,18 @@ function infoFilmSeen (req, res) {
 		if(user) {
 			console.log('idFilm: '+req.body.idFilm)
 			console.log('filmsWatched: '+user.filmsWatched)
-			user.filmsWatched.push(req.body.idFilm)
-			console.log('user: '+user)
-			var query = {name: req.body.nameUser};
-			var update = { $set: { filmsWatched: user.filmsWatched }}
-			User.update(query, update, {upsert:true}, function(err, doc){
-				if (err) return res.send(500, { error: err });
-				return res.send("succesfully saved");
-			});
+			var filmExsist = user.filmsWatched.indexOf(req.body.idFilm);
+			console.log('**********************************'+filmExsist)
+			if(filmExsist===-1){
+				user.filmsWatched.push(req.body.idFilm)
+				console.log('user: '+user)
+				var query = {name: req.body.nameUser};
+				var update = { $set: { filmsWatched: user.filmsWatched }}
+				User.update(query, update, {upsert:true}, function(err, doc){
+					if (err) return res.send(500, { error: err });
+					return res.send("succesfully saved");
+				});
+			}
 		}
 	})
 }
